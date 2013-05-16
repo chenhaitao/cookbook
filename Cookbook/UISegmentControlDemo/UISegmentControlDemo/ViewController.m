6e2be640a95a0a4e276b533e974d5de57ecf48c1
陈海涛 <cht005288@gmail.com>
2013-05-16 20:19:17 +0800

    add UIsegmentedControl

--- Cookbook/UISegmentControlDemo/UISegmentControlDemo/ViewController.m
+++ Cookbook/UISegmentControlDemo/UISegmentControlDemo/ViewController.m
@@ -30,10 +30,32 @@
 {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
-    NSArray *segments = [[NSArray alloc] initWithObjects:@"iphone",@"ipad",@"ipod",@"imac", nil];
+  //  NSArray *segments = [[NSArray alloc] initWithObjects:@"iphone",@"ipad",@"ipod",@"imac", nil];
+    //显示图片
+    NSArray *segments = [[NSArray alloc] initWithObjects:@"iphone",[UIImage imageNamed:@"iPad"],@"iPod",@"iMac", nil];
+    
     self.mySegmentedControl = [[UISegmentedControl alloc] initWithItems:segments];
+    CGRect frame = self.mySegmentedControl.frame;
+    frame.size.width = 300.0f;
+    frame.size.height = 64.0f;
+    self.mySegmentedControl.frame = frame;
     self.mySegmentedControl.center = self.view.center;
     [self.view addSubview:self.mySegmentedControl];
+    
+    //用户在选中一个选项之后，该选项马上变回原始状态
+    self.mySegmentedControl.momentary = YES;
+    
+    /*
+     typedef NS_ENUM(NSInteger, UISegmentedControlStyle) {
+     UISegmentedControlStylePlain,     // large plain 默认风格
+     UISegmentedControlStyleBordered,  // large bordered
+     UISegmentedControlStyleBar,       // small button/nav bar style. tintable
+     UISegmentedControlStyleBezeled,   // DEPRECATED. Do not use this style.
+     };
+     */
+    self.mySegmentedControl.segmentedControlStyle = UISegmentedControlStyleBar;
+    
+    [self.mySegmentedControl addTarget:self action:@selector(segmentControlChagned:) forControlEvents:UIControlEventValueChanged];
     
 }
 
